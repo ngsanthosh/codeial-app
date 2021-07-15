932f1acee50e7ec07535bf60f4ea7d6debbf4cc7
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-15 12:53:15 +0530

    form handling with controlled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class Login extends Component {
+    constructor(props){
+        super(props)
+        this.a = createRef();
+        this.b = createRef();
+    }
+    clickDone = (e) => {
+       e.preventDefault()
+       console.log(this.a.current.value);
+       console.log(this.b.current.value);
+   };
   render() {
     return (
       <div>
         <form className="login-form">
           <span className="login-signup-header">Log In</span>
           <div className="field">
-            <input type="email" placeholder="Email" required />
+            <input type="email" placeholder="Email" required ref={this.a} />
           </div>
           <div className="field">
-            <input type="password" placeholder="Password" required />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              ref={this.b}
+            />
           </div>
           <div className="field">
-            <button>Log In</button>
+            <button onClick={this.clickDone}>Log In</button>
           </div>
         </form>
       </div>
