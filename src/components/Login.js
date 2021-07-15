8f07588e01d39cfeb0231ed375a696dd45f59505
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-15 14:54:36 +0530

    Form Handling using Controlled Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,48 @@
 import React, { Component, createRef } from "react";
 
 export default class Login extends Component {
-    constructor(props){
-        super(props)
-        this.a = createRef();
-        this.b = createRef();
-    }
-    clickDone = (e) => {
-       e.preventDefault()
-       console.log(this.a.current.value);
-       console.log(this.b.current.value);
-   };
+  constructor(props) {
+    super(props);
+    // this.a = createRef();
+    // this.b = createRef();
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+  handlePwdChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+  clickDone = (e) => {
+    e.preventDefault();
+    // console.log(this.a.current.value);
+    // console.log(this.b.current.value);
+    // this.setState({ username: this.a });
+    console.log(this.state);
+  };
   render() {
     return (
       <div>
         <form className="login-form">
           <span className="login-signup-header">Log In</span>
           <div className="field">
-            <input type="email" placeholder="Email" required ref={this.a} />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              onChange={this.handleEmailChange}
+            />
           </div>
           <div className="field">
             <input
               type="password"
               placeholder="Password"
               required
-              ref={this.b}
+              onChange={this.handlePwdChange}
             />
           </div>
           <div className="field">
