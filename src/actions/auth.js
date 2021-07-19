37fa480a6b45920f863784cd7f3093fd215ea5c1
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-19 12:56:39 +0530

    logging in user 3

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
 import { APIurls } from "../pleasehelpme/urls";
 import { getFormBody } from "../pleasehelpme/utils";
-import { LOGIN_START } from "./actionTypes";
+import { LOGIN_FAILURE, LOGIN_START, LOGIN_SUCCESS } from "./actionTypes";
 
 export function startlogin() {
   return {
@@ -8,16 +8,66 @@ export function startlogin() {
   };
 }
 
-export function login() {
+export function loginsuccess(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    user,
+  };
+}
+
+export function loginfailed(error) {
+  return {
+    type: LOGIN_FAILURE,
+    error,
+  };
+}
+
+export function login(email, password) {
   return (dispatch) => {
-    const url = APIurls.login();
+    dispatch(startlogin());
+    const url = APIurls.signup();
 
     fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: getFormBody({email, password})
-    });
+      body: getFormBody({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //   dispatch()
+        if (data.success) {
+          dispatch(loginsuccess(data.data.user));
+          return;
+        }
+        dispatch(loginfailed(data.message));
+      });
   };
 }
+
+// export function signup(email, name, password, confirm_password) {
+//     return (dispatch) => {
+//     //   dispatch(startlogin());
+//       const url = APIurls.signup();
+  
+//       fetch(url, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: getFormBody({ email, password }),
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           console.log(data);
+//           //   dispatch()
+//           if (data.success) {
+//             dispatch(loginsuccess(data.data.user));
+//             return;
+//           }
+//           dispatch(loginfailed(data.message));
+//         });
+//     };
+//   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { Component, createRef } from "react";
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
 
-export default class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
     // this.a = createRef();
@@ -10,7 +12,10 @@ export default class Login extends Component {
       password: "",
     };
   }
-
+  // componentWillUnmount() {
+  //   let { error } = this.props.auth;
+  //   error = null;
+  // }
   handleEmailChange = (e) => {
     this.setState({ username: e.target.value });
   };
@@ -22,12 +27,20 @@ export default class Login extends Component {
     // console.log(this.a.current.value);
     // console.log(this.b.current.value);
     // this.setState({ username: this.a });
+
+    const { username, password } = this.state;
+    const { dispatch } = this.props;
+    if (username && password) {
+      dispatch(login(username, password));
+    }
     console.log(this.state);
   };
   render() {
+    const { error, inprogress } = this.props.auth;
     return (
       <div>
         <form className="login-form">
+          {error && <div className="alert error-dailog">{error}</div>}
           <span className="login-signup-header">Log In</span>
           <div className="field">
             <input
@@ -46,10 +59,23 @@ export default class Login extends Component {
             />
           </div>
           <div className="field">
-            <button onClick={this.clickDone}>Log In</button>
+            {inprogress ? (
+              <button onClick={this.clickDone} disabled={inprogress}>
+                Logging in...
+              </button>
+            ) : (
+              <button onClick={this.clickDone}>Log In</button>
+            )}
           </div>
         </form>
       </div>
     );
   }
 }
+
+const mapStatetoProps = ({ auth }) => {
+  return {
+    auth,
+  };
+};
+export default connect(mapStatetoProps)(Login);
