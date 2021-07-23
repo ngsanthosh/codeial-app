9b82eb6c0cfb12800e7905672aefb0b48e0fa7fe
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-23 12:57:23 +0530

    Modified search reducer

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,11 +1,11 @@
 import { FETCH_USER_FROM_SEARCH } from "../actions/actionTypes";
 
-export function search(state={ results:[] }, action){
-    switch(action.type){
-        case FETCH_USER_FROM_SEARCH:
-            return{
-                ...state,
-                results: action.users
-            }
-    }
+export function search(state = { results: [] }, action) {
+  switch (action.type) {
+    case FETCH_USER_FROM_SEARCH:
+      return {
+        ...state,
+        results: action.users,
+      };
+  }
 }
