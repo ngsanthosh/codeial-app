f645a351c2d16970c9059258ccc49f77cbf9b437
ngsanthosh <ngsanthosh657@gmail.com>
2021-10-05 19:25:16 +0530

    Revert changes

--- src/components/Wrongpath.jsx
+++ src/components/Wrongpath.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Wrongpath() {
     return (
         <div>
-            <h1 className="login-form">404: Page Not Found...</h1>
+            <h1 className="login-form">404: Page Not Found</h1>
         </div>
     )
 }
