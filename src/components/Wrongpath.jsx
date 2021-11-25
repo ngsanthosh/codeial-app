0d83d3146ca35999ca710e9de220f5033c98ad79
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-25 19:52:40 +0530

    Update Wrongpath.jsx

--- src/components/Wrongpath.jsx
+++ src/components/Wrongpath.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Wrongpath() {
     return (
         <div>
-            <h1 className="login-form">404: Page Not Found</h1>
+            <h1 className="login-form">404: Page Not Found </h1>
+            <h1 className="login-form">Check with the URL! </h1>
         </div>
     )
 }
