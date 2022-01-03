10b1ea6cd77f288f3ff0d800ec7255cd174d7579
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-03 19:56:14 +0530

    Error fix - Submitting signup without data

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,10 +8,10 @@ class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: " ",
-      email: " ",
-      password: " ",
-      confirmPassword: " ",
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     };
   }
   componentDidMount() {
